a47d8291c9e70d7b4303d24660b22d92b7aec192
zhangir-azerbayev <zazerbayev@gmail.com>
2024-06-29 13:23:34 -0700

    update fish, no longer use conda

--- config.fish
+++ config.fish
@@ -1,20 +1,5 @@
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /home/zhangir/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
-# aliased ssh commands 
-# see `tmux` section for `rename` command
-alias ziva="rename ziva; ssh -o ServerAliveInterval=30 -J zaa7@tangra.cs.yale.edu zaa7@ziva.cs.yale.internal"
-alias morana="rename morana; ssh -o ServerAliveInterval=30 -J zaa7@tangra.cs.yale.edu zaa7@morana.cs.yale.edu"
-alias tangra="rename tangra; ssh -o ServerAliveInterval=30 zaa7@tangra.cs.yale.edu"
-alias vesna="rename vesna; ssh -o ServerAliveInterval=30 -J zaa7@tangra.cs.yale.edu zaa7@vesna.cs.yale.internal"
-alias stability="rename stability; ssh -i ~/.ssh/id_ed25519 -o ServerAliveInterval=30 zhangir.azerbayev@welcomewest.hpc.stability.ai"
-alias byu="rename byu; ssh -o ServerAliveInterval=30 za2514@ssh.rc.byu.edu"
-alias mounttangra="rename tangra-files; sshfs -o ServerAliveInterval=30 zaa7@tangra.cs.yale.edu:/home/zaa7/ ~/tangra"
-alias mountstability="rename stability-files; sshfs -o ServerAliveInterval=30 zhangir.azerbayev@grantwest.hpc.stability.ai:/fsx/proj-mathlm/ ~/grantwest"
-alias mountbyu="rename byu-files; sshfs -o ServerAliveInterval=30 za2514@ssh.rc.byu.edu:/nobackup/scratch/usr/za2514/ ~/byu"
+# vi mode in fish
+fish_vi_key_bindings
 
 # nvim 
 alias vim="nvim"
@@ -23,12 +8,6 @@ alias oldvim="/bin/vim"
 
 # exa
 alias ls="exa"
-
-# tmux
-alias rename="tmux rename-window"
-
-# don't remember why I have this, it might be for Dafny
-set -gx PATH $PATH $HOME/.dotnet/tools
 
 # api keys
 source ~/.config/fish/api_keys.fish
